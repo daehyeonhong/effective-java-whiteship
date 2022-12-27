8050f8191ac95dad70fa0e6d1c262b0e39d680b4
DaeHyeonHong <fholng@outlook.com>
2022-12-27 16:40:05 +0900

    initial

--- src/main/java/me/whiteship/chapter01/item01/Settings.java
+++ src/main/java/me/whiteship/chapter01/item01/Settings.java
@@ -1,22 +1,7 @@
 package me.whiteship.chapter01.item01;
 
-/**
- * 이 클래스의 인스턴스는 #getInstance()를 통해 사용한다.
- *
- * @see #getInstance()
- */
 public class Settings {
-
-    private static final Settings SETTINGS = new Settings();
     private boolean useAutoSteering;
     private boolean useABS;
     private Difficulty difficulty;
-
-    private Settings() {
-    }
-
-    public static Settings getInstance() {
-        return SETTINGS;
-    }
-
 }
