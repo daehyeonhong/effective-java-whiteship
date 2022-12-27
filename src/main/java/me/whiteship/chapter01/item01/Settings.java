1176a1cd1a5f9e381eba95ed0b42845edb62ceee
DaeHyeonHong <fholng@outlook.com>
2022-12-27 16:45:48 +0900

    static factory 생성을 제어할 수 있다.;

--- src/main/java/me/whiteship/chapter01/item01/Settings.java
+++ src/main/java/me/whiteship/chapter01/item01/Settings.java
@@ -4,4 +4,13 @@ public class Settings {
     private boolean useAutoSteering;
     private boolean useABS;
     private Difficulty difficulty;
+
+    private Settings() {
+    }
+
+    private static final Settings SETTINGS = new Settings();
+
+    public static Settings newInstance() {
+        return SETTINGS;
+    }
 }
