de00221fadba73ff4b52869ce6c40083c679ff61
DaeHyeonHong <fholng@outlook.com>
2022-12-27 16:50:51 +0900

    init

--- src/main/java/me/whiteship/chapter01/item01/HelloService.java
+++ src/main/java/me/whiteship/chapter01/item01/HelloService.java
@@ -1,28 +1,5 @@
 package me.whiteship.chapter01.item01;
 
 public interface HelloService {
-
-    static String hi() {
-        prepareMessage();
-        return "hi";
-    }
-
-    static private void prepareMessage() {
-    }
-
-    static String hi1() {
-        prepareMessage();
-        return "hi";
-    }
-
-    static String hi2() {
-        prepareMessage();
-        return "hi";
-    }
-
-    String hello();
-
-    default String bye() {
-        return "bye";
-    }
+    String hi();
 }
--- src/main/java/me/whiteship/chapter01/item01/HelloServiceFactory.java
+++ src/main/java/me/whiteship/chapter01/item01/HelloServiceFactory.java
@@ -4,4 +4,9 @@ public class HelloServiceFactory {
     public static HelloService of(final String lang) {
         return ("ko".equals(lang)) ? new KoreanHelloService() : new EnglishHelloService();
     }
+
+    public static void main(String[] args) {
+        final HelloService ko = HelloServiceFactory.of("ko");
+        ko.hello();
+    }
 }
