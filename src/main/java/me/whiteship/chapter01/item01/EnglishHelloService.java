10cc389d88f412407c0342195452c618cdaab83e
DaeHyeonHong <fholng@outlook.com>
2022-12-27 16:48:45 +0900

    init

---
+++ src/main/java/me/whiteship/chapter01/item01/EnglishHelloService.java
@@ -0,0 +1,8 @@
+package me.whiteship.chapter01.item01;
+
+public class EnglishHelloService implements HelloService {
+    @Override
+    public String hello() {
+        return "Hello";
+    }
+}
---
+++ src/main/java/me/whiteship/chapter01/item01/KoreanHelloService.java
@@ -0,0 +1,8 @@
+package me.whiteship.chapter01.item01;
+
+public class KoreanHelloService implements HelloService {
+    @Override
+    public String hello() {
+        return "안녕";
+    }
+}
